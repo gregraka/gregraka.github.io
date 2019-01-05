7c29a7e49b411f8a67b3247c06311c567df06b25
Greg Rakauskas <46380033+gregraka@users.noreply.github.com>
2019-01-05 15:17:29 -0500

    Further testing.

--- markdown_source/convert_to_html.bat
+++ markdown_source/convert_to_html.bat
@@ -1,8 +1,22 @@
+echo off
 
-
+REM Make sure that we're on the right drive.
 C:
+
+
+REM Change dir to Perl bin.
 cd \Perl64\bin
 
+
+REM Convert markdown to html.
+echo on
 perl Markdown.pl --html4tags \Users\grraka\Documents\GitHub\gregraka.github.io\markdown_source\test.txt > \Users\grraka\Documents\GitHub\gregraka.github.io\test.html
 perl Markdown.pl --html4tags \Users\grraka\Documents\GitHub\gregraka.github.io\markdown_source\test2.txt > \Users\grraka\Documents\GitHub\gregraka.github.io\test2.html
+echo off
+REM Add new line above 'echo off.'
 
+
+echo --------------------------------
+echo %DATE%  %TIME%
+echo ________________________________
+pause
