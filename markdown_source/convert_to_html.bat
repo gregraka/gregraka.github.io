9f0105ceb9734b5f47c200b94bbead5dbf45d8b0
Greg Rakauskas <46380033+gregraka@users.noreply.github.com>
2019-01-05 14:54:06 -0500

    Addition of a second HTML file converted from markdown.

---
+++ markdown_source/convert_to_html.bat
@@ -0,0 +1,8 @@
+
+
+C:
+cd \Perl64\bin
+
+perl Markdown.pl --html4tags \Users\grraka\Documents\GitHub\gregraka.github.io\markdown_source\test.txt > \Users\grraka\Documents\GitHub\gregraka.github.io\test.html
+perl Markdown.pl --html4tags \Users\grraka\Documents\GitHub\gregraka.github.io\markdown_source\test2.txt > \Users\grraka\Documents\GitHub\gregraka.github.io\test2.html
+
